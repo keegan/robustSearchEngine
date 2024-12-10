ffcf83a4fb8c8771e81701d9d70cb5ada38fdb1f
keegan <swfanboy01@gmail.com>
2024-12-10 14:58:07 +0100

    basic fuzzy search

--- src/main.py
+++ src/main.py
@@ -1,12 +1,34 @@
-import pymupdf, sys, pathlib
+import pymupdf, sys, pathlib, os, glob
+from fuzzywuzzy import fuzz
 
 def main():
-  filename = pathlib.Path(sys.argv[1])
-  print(filename)
-  with pymupdf.open(filename) as document:
-    text = chr(12).join([page.get_text() for page in document])
-  print(text)
+  searchDir = pathlib.Path(sys.argv[1])
+  print(searchDir)
+  directory = os.path.abspath(searchDir)
+  pdf_files = glob.glob(os.path.join(directory, "**", "*.pdf"), recursive=True)
+  # files = []
+  # for path, _, files in os.walk(searchDir):
+  #   print(files)
+  #   for name in files:
+  #     print(name)
+  #     if name.endswith('.pdf'):
+  #       files.append(pathlib.PurePath(path, name))
+  #   print("--")
+        
+  print(pdf_files)
+  lookup = {}
+  for f in pdf_files:
+    lookup[f] = []
+    with pymupdf.open(f) as document:
+      for page in document:
+        lookup[f].append(page.get_text())
 
-
+  while True:
+    search = input("Search Phrase: ")
+    for file in lookup.keys():
+      for pagenum, page  in enumerate(lookup[file]):
+        m = fuzz.partial_ratio(page.lower(), search.lower())
+        if m > 70:
+          print(f'{file} : {pagenum} : {m}')  
 if __name__ == '__main__':
   main()
